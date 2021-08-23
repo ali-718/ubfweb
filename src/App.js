afb996147f98df19717aae07b0a05589a0f33a10
ali-718 <alimurtuza718@gmail.com>
2021-08-23 20:19:48 +0500

    testing map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes } from "./routes";
+import CountiesChart from "./test";
 
 export const App = () => {
-  return <Routes />;
+  return <CountiesChart />;
 };
